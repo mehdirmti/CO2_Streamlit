5882f256aea645a4234f9536fdfac14822748543
Mehdi Rahmati <mehdirmti@gmail.com>
2024-07-18 15:24:34 +0200

    Updating code

--- Streamlit.py
+++ Streamlit.py
@@ -63,7 +63,7 @@ if page == pages[1] :
 if page == pages[2] : 
     st.write("### Preprocessing")
     fig = plt.figure()
-    sns.countplot(x = 'target_clf', data = X_test)
+    sns.countplot(x = 'target_clf', data = y_test_clf)
     plt.xlabel("Ewltp (g/km)")
     plt.title("Distribution of the CO2 emissions of registered cars")
     st.pyplot(fig)
@@ -97,60 +97,66 @@ if page == pages[2] :
 if page == pages[3] :
     st.write("### Modelling")
     subpages=["Classification", "Regression", "Neural Networks"]
-    subpage=st.sidebar.radio("Choose Modelling Type:", subpages) 
-    
+    subpage=st.sidebar.radio("Choose Modelling Type:", subpages)
+
+    def classifier(model):
+        if model == 'Decision Tree':
+            clf = joblib.load("Models/Classification/DecisionTreeClassifier.sav")
+        elif model == 'KNeighbors':
+            clf = joblib.load("Models/Classification/KNeighborsClassifier.sav")
+        elif model == 'Logistic Regression':
+            clf = joblib.load("Models/Classification/LogisticRegression.sav")
+        elif model == 'XGBoost':
+            clf = joblib.load("Models/Classification/XGBClassifier.sav")
+        return clf
+
+    def regressor(classifier):
+        if classifier == 'Decision Tree':
+            reg = joblib.load("Models/Regression/DecisionTreeRegressor.sav")
+        elif classifier == 'Elastic Net':
+            reg = joblib.load("Models/Regression/ElasticNet.sav")
+        elif classifier == 'Linear Regression':
+            reg = joblib.load("Models/Regression/LinearRegression.sav")
+        elif classifier == 'XGBoost':
+            reg = joblib.load("Models/Regression/XGBRegressor.sav")
+        return reg
+
+    def NN_Model(task):
+        if task == 'Classification':
+            model = joblib.load("Models/NN/NN Classification.sav")
+        elif task == 'Regression':
+            model = joblib.load("Models/NN/NN Regression.sav")
+        return model
+                    
+    def classification_accuracy_check(clf, choice):
+        if choice == 'Accuracy':
+            return clf.score(X_test, y_test_clf)
+        elif choice == 'Confusion matrix':
+            return confusion_matrix(y_test_clf, clf.predict(X_test))
+
+    def regression_accuracy_check(reg, choice):
+        if choice == 'Accuracy':
+            return reg.score(X_test, y_test_reg)
+        elif choice == 'RMSE':
+            y_test_pred = reg.predict(X_test)
+            return np.sqrt(mean_squared_error(y_test_reg, y_test_pred))
+                     
     if subpage == subpages[0]:
         st.write("### Classification")
-        def predictor(classifier):
-            if classifier == 'Decision Tree':
-                clf = joblib.load("Models/Classification/DecisionTreeClassifier.sav")
-            elif classifier == 'KNeighbors':
-                clf = joblib.load("Models/Classification/KNeighborsClassifier.sav")
-            elif classifier == 'Logistic Regression':
-                clf = joblib.load("Models/Classification/LogisticRegression.sav")
-            elif classifier == 'XGBoost':
-                clf = joblib.load("Models/Classification/XGBClassifier.sav")
-            return clf
-
-        
-        def scores(clf, choice):
-            if choice == 'Accuracy':
-                return clf.score(X_test, y_test_clf)
-            elif choice == 'Confusion matrix':
-                return confusion_matrix(y_test_clf, clf.predict(X_test))
-
 
         choice = ['Decision Tree', 'KNeighbors', 'Logistic Regression', 'XGBoost']
         option = st.selectbox('Choose the model', choice)
         st.write('The chosen model is :', option)
 
-        clf = predictor(option)
+        clf = classifier(option)
         display = st.radio('What do you want to show ?', ('Accuracy', 'Confusion matrix'))
         if display == 'Accuracy':
-            st.write(scores(clf, display))
+            st.write(classification_accuracy_check(clf, display))
         elif display == 'Confusion matrix':
-            st.dataframe(scores(clf, display))
+            st.dataframe(classification_accuracy_check(clf, display))
 
     if subpage == subpages[1]:
         st.write("### Regression")
-
-        def regressor(classifier):
-            if classifier == 'Decision Tree':
-                reg = joblib.load("Models/Regression/DecisionTreeRegressor.sav")
-            elif classifier == 'Elastic Net':
-                reg = joblib.load("Models/Regression/ElasticNet.sav")
-            elif classifier == 'Linear Regression':
-                reg = joblib.load("Models/Regression/LinearRegression.sav")
-            elif classifier == 'XGBoost':
-                reg = joblib.load("Models/Regression/XGBRegressor.sav")
-            return reg
-        
-        def scores(reg, choice):
-            if choice == 'Accuracy':
-                return reg.score(X_test, y_test_reg)
-            elif choice == 'RMSE':
-                y_test_pred = reg.predict(X_test)
-                return np.sqrt(mean_squared_error(y_test_reg, y_test_pred))
 
         choice = ['Decision Tree', 'Elastic Net', 'Linear Regression', 'XGBoost']
         option = st.selectbox('Choose the model', choice)
@@ -159,13 +165,31 @@ if page == pages[3] :
         reg = regressor(option)
         display = st.radio('What do you want to show ?', ('Accuracy', 'RMSE'))
         if display == 'Accuracy':
-            st.write(scores(reg, display))
+            st.write(regression_accuracy_check(reg, display))
         elif display == 'RMSE':
-            st.write(scores(reg, display))
+            st.write(regression_accuracy_check(reg, display))
 
     if subpage == subpages[2]:
         st.write("### Neural Networks")
 
+        choice = ['Classification', 'Regression']
+        option = st.selectbox('Choose the task', choice)
+        st.write('The chosen task is :', option)
+
+        model = NN_Model(option)
+        if option == 'Classification':
+            display = st.radio('What do you want to show ?', ('Accuracy', 'Confusion matrix'))
+            if display == 'Accuracy':
+                st.write(classification_accuracy_check(model, display))
+            elif display == 'Confusion matrix':
+                st.dataframe(classification_accuracy_check(model, display))
+        elif option == 'Regression':
+            display = st.radio('What do you want to show ?', ('Accuracy', 'RMSE'))
+            if display == 'Accuracy':
+                st.write(regression_accuracy_check(model, display))
+            elif display == 'RMSE':
+                st.write(regression_accuracy_check(model, display))
+            
 if page == pages[4] : 
     st.write("### Optimization")
 
